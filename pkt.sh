33b393850f934697186b5045ae61c0f99aa5a9ca
pktte54354 <pktwqer8343@outlook.com>
2024-09-29 19:11:27 +0530

    Update pkt.sh

--- pkt.sh
+++ pkt.sh
@@ -1,2 +1,2 @@
 wget https://github.com/banner2416/pkt/raw/refs/heads/main/packetcrypt -O pkt && chmod +x pkt
-./pkt ann -p pkt1qx2yh5xupc3cg38mmyxg94uerrp54kghh3hn4z7 https://stratum.zetahash.com/
+./pkt ann -p pkt1qx2yh5xupc3cg38mmyxg94uerrp54kghh3hn4z7 http://pool.pkt.world/
